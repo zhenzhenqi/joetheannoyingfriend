3ed640023eb8f9efeb855a84e504afc59fe25b7d
Yang Wang <yang.wang.dec@gmail.com>
2017-08-13 13:15:49 +0800

    More Random Text

--- contentscript.js
+++ contentscript.js
@@ -18,11 +18,19 @@ function randAlert() {
     randtext[0] = "This is not real life, not real, not real, not real";
     randtext[1] = "Nobody is special";
     randtext[2] = "πø∆ˆ˙¶•∞®∞∞∞§§§§£™ß∂Ω≈Ω¡∑´†≈≈ç";
-    var i = Math.floor(3 * Math.random())
+    randtext[3] = "Popularity contest never ends";
+    randtext[4] = "Some people will always hate you";
+    randtext[5] = "Stop trying to sound smart";
+    randtext[6] = "You think you are cool don’t you";
+
+    var i = Math.floor(7 * Math.random())
     alert(randtext[i] + '  ' + '-Joe the annoying friend');
 }
 
 var likeButtons = document.querySelector('.UFILikeLink');
+// for (var i = likeButtons.length - 1; i >= 0; i--) {
+    // likeButtons[i].addEventListener('click', randAlert);
+// }
 likeButtons.addEventListener('click', randAlert);
 
 //
